763e96af8045bb825d5c84c82146a1057bda6397
Osman Shaikh <78846955+osmanshaikhSystemsltd@users.noreply.github.com>
2021-10-15 18:11:21 +0500

    Update Devops_library.groovy

--- Devops_library.groovy
+++ Devops_library.groovy
@@ -1,4 +1,4 @@
-def call(String project, String hubUser, String credentialsId) {
+def docker_image(String project, String hubUser, String credentialsId) {
     sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
     withCredentials([usernamePassword(
             credentialsId: credentialsId,

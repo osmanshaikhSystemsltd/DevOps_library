6807d04ee1def765106c6b17364b9c903a170a02
Osman Shaikh <78846955+osmanshaikhSystemsltd@users.noreply.github.com>
2021-10-17 00:16:30 +0500

    Create dockerPushFn.groovy

---
+++ vars/dockerPushFn.groovy
@@ -0,0 +1,11 @@
+def call(String project, String hubUser, String credentialsId) {
+    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+    withCredentials([usernamePassword(
+            credentialsId: credentialsId,
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
+    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+}

efca2ed45d0bd380586eade6625e03b93b0ea129
Osman Shaikh <78846955+osmanshaikhSystemsltd@users.noreply.github.com>
2021-10-16 23:53:36 +0500

    docker-push

--- vars/Devops_library.groovy
+++ vars/Devops_library.groovy
@@ -6,14 +6,14 @@ def print(String msg){
     echo ${msg}
 }
 
-// def dockerPush(String project, String hubUser, String credentialsId) {
-//     sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
-//     withCredentials([usernamePassword(
-//             credentialsId: credentialsId,
-//             usernameVariable: "USER",
-//             passwordVariable: "PASS"
-//     )]) {
-//         sh "docker login -u '$USER' -p '$PASS'"
-//     }
-//     sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
-// }
+def dockerPush(String project, String hubUser, String credentialsId) {
+    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+    withCredentials([usernamePassword(
+            credentialsId: credentialsId,
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
+    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+}

88b91afa47f547547febfa5b785d36b940e5e2f9
Osman Shaikh <78846955+osmanshaikhSystemsltd@users.noreply.github.com>
2021-10-16 23:23:11 +0500

    print function

--- Devops_library.groovy
+++ Devops_library.groovy
@@ -1,11 +1,15 @@
-def dockerPush(String project, String hubUser, String credentialsId) {
-    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
-    withCredentials([usernamePassword(
-            credentialsId: credentialsId,
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
-    }
-    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+def print(String msg){
+    echo ${msg}
 }
+
+// def dockerPush(String project, String hubUser, String credentialsId) {
+//     sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+//     withCredentials([usernamePassword(
+//             credentialsId: credentialsId,
+//             usernameVariable: "USER",
+//             passwordVariable: "PASS"
+//     )]) {
+//         sh "docker login -u '$USER' -p '$PASS'"
+//     }
+//     sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+// }

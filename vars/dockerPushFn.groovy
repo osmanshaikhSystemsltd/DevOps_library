00f41e8577db98c019cfa1748a89ddb155ec9eb4
Osman Shaikh <78846955+osmanshaikhSystemsltd@users.noreply.github.com>
2021-10-26 19:12:11 +0500

    Update dockerPushFn.groovy

--- vars/dockerPushFn.groovy
+++ vars/dockerPushFn.groovy
@@ -1,6 +1,6 @@
 def call(String project, String hubUser, String credentialsId) {
     sshagent(['ssh-jenkins-build-machine']) {
     // some block
-        sh 'ssh -o UserKnownHostsFile=/dev/null osmanshaikh@20.54.72.51'
+        sh 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no osmanshaikh@20.54.72.51'
     }
 }

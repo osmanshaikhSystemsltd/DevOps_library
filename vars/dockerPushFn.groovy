9fb752b6175917c373dd867cbfef76b3e9c0bb1d
Osman Shaikh <78846955+osmanshaikhSystemsltd@users.noreply.github.com>
2021-10-26 18:35:29 +0500

    Update dockerPushFn.groovy

--- vars/dockerPushFn.groovy
+++ vars/dockerPushFn.groovy
@@ -1,11 +1,15 @@
 def call(String project, String hubUser, String credentialsId) {
-    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
-    withCredentials([usernamePassword(
-            credentialsId: credentialsId,
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
+    sshagent(['ssh-jenkins-build-machine']) {
+    // some block
+        sh 'ssh osmanshaikh@20.54.72.51'
+        sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+        withCredentials([usernamePassword(
+                credentialsId: credentialsId,
+                usernameVariable: "USER",
+                passwordVariable: "PASS"
+        )]) {
         sh "docker login -u '$USER' -p '$PASS'"
     }
     sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+    }
 }

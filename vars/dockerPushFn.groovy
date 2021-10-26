8a102c467f53f5d474a5b0cea23d67cc7903094d
Osman Shaikh <78846955+osmanshaikhSystemsltd@users.noreply.github.com>
2021-10-26 19:21:32 +0500

    Update dockerPushFn.groovy

--- vars/dockerPushFn.groovy
+++ vars/dockerPushFn.groovy
@@ -2,8 +2,8 @@ def call(String project, String hubUser, String credentialsId) {
     sshagent(['ssh-jenkins-build-machine']) {
     // some block
         sh 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no osmanshaikh@20.54.72.51'
-        sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no osmanshaikh@20.54.72.51 'whoami'
-        sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no osmanshaikh@20.54.72.51 'pwd'
-        sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no osmanshaikh@20.54.72.51 'ls'
+        sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no osmanshaikh@20.54.72.51 'whoami'"
+        sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no osmanshaikh@20.54.72.51 'pwd'"
+        sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no osmanshaikh@20.54.72.51 'ls'"
     }
 }

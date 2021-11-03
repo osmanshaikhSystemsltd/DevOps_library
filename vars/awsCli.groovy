59e7c70390940554aba9e515cf9999c95ab30c37
Osman Shaikh <78846955+osmanshaikhSystemsltd@users.noreply.github.com>
2021-11-03 17:10:59 +0500

    Update awsCli.groovy

--- vars/awsCli.groovy
+++ vars/awsCli.groovy
@@ -1,5 +1,8 @@
 def call(String cmd, String creds) {
     sshagent([creds]) {
+        sh 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null systemsltd@20.74.128.141 echo "<<<<GIT>>>>"
+        sh 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null systemsltd@20.74.128.141 git --version'
+                sh 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null systemsltd@20.74.128.141 echo "<<<<CMD>>>>"
         sh 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null systemsltd@20.74.128.141 $cmd'
     }
 }
